88a83140d6762009d2e919aa6f870fa09859f201
Wizbaggd <Wizbaggd@gmail.com>
2018-11-05 13:30:54 +0800

    Kill Feed
    Added Kill Feed

--- scripts/4_World/Entities/ManBase/PlayerBase/SurvivorBase.c
+++ scripts/4_World/Entities/ManBase/PlayerBase/SurvivorBase.c
@@ -1,4 +1,11 @@
-class SurvivorBase extends PlayerBaseClient
-{
-	
+class SurvivorBase extends PlayerBaseClient {
+    private string PlayerIdentityName = "empty";
+
+    string GetPlayerIdentityName() {
+        return this.PlayerIdentityName;
+    }
+
+    void SetPlayerIdentityName(string name) {
+        this.PlayerIdentityName = name;
+    }
 }
